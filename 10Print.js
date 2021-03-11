c7d80833549979e90cdedd7bff4b3888daef05cd
patrick765418 <patrickbadea@yahoo.com>
2021-03-11 16:11:54 +0200

    qa!
    

---
+++ 10Print.js
@@ -0,0 +1,68 @@
+let xRight = window.innerWidth;
+let xLeft = 0;
+let y = 0;
+let spacing = 60;
+let epochs = 0;
+let deviaton = 0;
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  // createCanvas(window.innerHeight / 2,window.innerHeight / 2);
+}
+
+function draw() {
+  // background(0,225,225);
+  // for (let i = 0; i < 100; i++) {
+  //     // if(i%5 == 0)
+
+  //     // setTimeout(() => {  console.log("World!");background(0,255,255,5); }, 2000);
+  //     // setTimeout(() => {  console.log("Hello!");background(0,255,255,5); }, 1000);
+  // }
+  var colors = ["#18F265", "#0b7d33", "#809c89", "#04e051", "#0b8a37"];
+  // var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  // colors.push(randomColor);
+  // randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  // colors.push(randomColor);
+  // randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  // colors.push(randomColor);
+
+  stroke(colors[Math.floor(random(1) * 10) % colors.length]);
+  // print(colors[Math.floor(random(1) * 10) % 5])
+  var probability = 0.8;
+  if (random(1) < probability) {
+    line(xLeft, y, xLeft + spacing, y + spacing);
+  } else {
+    line(xLeft, y + spacing, xLeft + spacing, y);
+  }
+  xLeft = xLeft + spacing;
+  if (xLeft > width / 2 - spacing) {
+    xLeft = 0;
+    y += spacing;
+  }
+
+  if (random(1) < probability - 0.63) {
+    line(xRight, y, xRight - spacing, y + spacing);
+  } else {
+    line(xRight, y + spacing, xRight - spacing, y);
+  }
+  xRight = xRight - spacing;
+  if (xRight < width / 2 + spacing) {
+    xRight = width;
+    // y += spacing;
+  }
+  if (y > height) {
+    let xRight = window.innerWidth;
+    let xLeft = 0;
+    y = 0;
+    background(0);
+    
+    epochs ++;
+    if(epochs == 3){
+      epochs = 0;
+
+      // deviaton++;
+      // xRight = xRight*deviaton/13;
+    }
+
+  }
+
+}
